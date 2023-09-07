fd23778f8eaf843504cd3ffbfd4f2daf8884be38
Varelazi <ilenevarela03@gmail.com>
2023-09-07 14:41:47 -0700

    First four challenges done

---
+++ classes-ilene-louis.js
@@ -0,0 +1,38 @@
+// Coffee Maker: copy the given Coffee class into a text editor
+// Write the code that makes a black coffee object
+// Write the code that outputs the black coffee's profile
+// Write the code that makes a coffee object with 1 cream and 2 sugars
+// Write the code that outputs the 1 cream and 2 sugars coffee profile
+
+class Coffee {
+  constructor(type, cream, sugar) {
+    this.type = type.toLowerCase()
+    this.cream = cream
+    this.sugar = sugar
+  }
+
+  coffeeProfile() {
+    return `A ${this.type} coffee with ${this.creams()}, ${this.sugars()}`
+  }
+
+  creams() {
+    if (this.cream > 1) {
+      return `${this.cream} creams`
+    } else {
+      return `${this.cream} cream`
+    }
+  }
+
+  sugars() {
+    if (this.sugar > 1) {
+      return `${this.sugar} sugars`
+    } else {
+      return `${this.sugar} sugar`
+    }
+  }
+}
+
+const blackCoffee = new Coffee("black coffee", 0, 0)
+console.log(blackCoffee)
+const coffeeOne = new Coffee("regular", 1, 2)
+console.log(coffeeOne)
