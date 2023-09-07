658630afdbdd750ef5352053bb07675159b5cd3b
tori-calkins <tori.calkins2705@gmail.com>
2023-09-07 15:04:07 -0700

    next section of answers

--- class-tori-mal.js
+++ class-tori-mal.js
@@ -47,3 +47,39 @@ class Coffee {
 // Write the code that outputs the 1 cream and 2 sugars coffee profile
 
 // console.log(coffeeDrink.coffeeProfile())
+
+
+
+
+// Latte Maker: create a class for Latte
+// Write a Latte class that takes a flavor, a milk type, and a number of shots
+
+class Latte {
+  constructor(flavor, milk, shots) {
+    this.flavor = flavor
+    this.milk = milk
+    this.shots = shots
+  }
+  latteProfile() {
+    return `A ${this.flavor} latte with ${this.milk} milk and ${this.shots} shot(s)`
+  }
+}
+
+// Write a method for your Latte class that outputs the latte's profile
+//latteProfile() {
+//   return `A ${this.flavor} latte with ${this.milk()}, ${this.shots()}`
+// }
+// Write the code that makes a regular, single shot latte
+const newLatte = new Latte("regular", "2%", 1)
+console.log(newLatte)
+// Log the regular, single shot latte's profile
+console.log(newLatte.latteProfile())
+// Write the code that makes a double shot, hazelnut latte with almond milk.
+const hazLatte = new Latte("hazelnut", "almond", 2)
+console.log(hazLatte)
+// Log the double shot, hazelnut latte with almond milk's profile.
+console.log(hazLatte.latteProfile())
+// Volume of a Cylinder: create a class for Cylinder
+// Write a class that calculates the volume of a Cylinder: v = πr^2h (r is the radius and h is the height of the cylinder)
+// Write the code that rounds the volume of the cylinder to four decimal places
+// Write the code that creates three unique cylinder objects
